f66e54f96588ec50e1ecebdd529e91c8b56ad82e
Alex Kent <alex.kent@ultraleap.com>
2024-03-28 19:18:01 +0800

    Extend the demo to cover switching out the whole UI.
    This effectively allows us to show full-window dialogs.

--- src/demo.c
+++ src/demo.c
@@ -8,6 +8,110 @@
 #define WIDTH 640
 #define HEIGHT 480
 
+int checked = 0;
+char buf[255] = {0};
+struct punk_style pb_style, btn_style, lbl_style;
+int dialog_visible = 0;
+int key_button_visible = 0;
+
+void show_gui()
+{
+  punk_begin_horizontal_layout("1:1:1:1", PUNK_FILL, PUNK_FILL);
+
+  punk_begin_vertical_layout("1:e20:e30:1:1:1", PUNK_FILL, PUNK_FILL);
+
+  // Tweak the style for the the picture box.
+  punk_picturebox("res/punk.png", &pb_style);
+  punk_checkbox("Checkmate", &checked, NULL);
+
+  if (punk_textbox(buf, NULL))
+  {
+    printf("Text changed: %s\n", buf);
+  }
+
+  punk_label("It's over", &lbl_style);
+  punk_label("9000!", &lbl_style);
+  if (punk_picture_button("res/punk.png", &pb_style))
+  {
+    printf("Punk!\n");
+    dialog_visible = 1;
+  }
+
+  punk_end_layout(); // Vertical layout
+
+  if (punk_button("hello", NULL)) printf("Hello world!\n");
+
+  if (key_button_visible)
+  {
+    if (punk_button("maybe", NULL)) printf("Maybe!\n");
+  }
+  else
+  {
+    punk_skip_layout_widget();
+  }
+
+  if (checked)
+  {
+    punk_begin_vertical_layout("1:2:1", PUNK_FILL, PUNK_FILL);
+
+    if (punk_button("V1", &btn_style) == PUNK_CLICK_RIGHT) printf("V1!\n");
+    if (punk_button("V2", &btn_style)) printf("V2!\n");
+    if (punk_button("V3", &btn_style)) printf("Punk!\n");
+
+    punk_end_layout(); // Vertical layout
+  }
+
+  punk_end_layout(); // Horizontal layout
+}
+
+struct CheckValue
+{
+  const char* field_name;
+  int checked;
+};
+
+struct CheckValue options[3] =
+{
+  {"Brachiosaurus", 1},
+  {"Velociraptor" , 0},
+  {"Aerodactyl"   , 0}
+};
+
+void show_dialog()
+{
+  punk_begin_vertical_layout("1:1:1:1:1", PUNK_FILL, PUNK_FILL);
+
+  punk_label("What is your favourite dinosaur?", NULL);
+  for (int i = 0; i < 3; i++)
+  {
+    const int initially_checked = options[i].checked;
+
+    punk_checkbox(options[i].field_name, &options[i].checked, NULL);
+
+    // I want these options to be mutually exclusive: if this one is checked
+    // then uncheck the other options.
+    if (options[i].checked)
+    {
+      options[(i + 1) % 3].checked = 0;
+      options[(i + 2) % 3].checked = 0;
+    }
+
+    // Additionally, do not allow the user to uncheck the only checkbox that
+    // is checked.
+    if (initially_checked && !options[i].checked)
+    {
+      options[i].checked = 1;
+    }
+  }
+
+  punk_begin_horizontal_layout("1:1", PUNK_FILL, PUNK_FILL);
+  if (punk_button("OK", NULL)) dialog_visible = 0;
+  if (punk_button("Cancel", NULL)) dialog_visible = 0;
+  punk_end_layout();
+
+  punk_end_layout();
+}
+
 int main()
 {
   int ret = SDL_Init(SDL_INIT_VIDEO);
@@ -31,11 +135,6 @@ int main()
 
   // Setup punk.
   punk_init(renderer, WIDTH, HEIGHT);
-
-  int key_button_visible = 0;
-  int checked = 0;
-  char buf[255] = {0};
-  struct punk_style pb_style, btn_style, lbl_style;
 
   punk_default_style(&pb_style);
   pb_style.back_colour_rgba = 0x0000FFFF;
@@ -71,55 +170,14 @@ int main()
     // Define the GUI here.
     punk_begin();
 
-    punk_begin_horizontal_layout("1:1:1:1", PUNK_FILL, PUNK_FILL);
-
-    punk_begin_vertical_layout("1:e20:e30:1:1:1", PUNK_FILL, PUNK_FILL);
-
-    // Tweak the style for the the picture box.
-    punk_picturebox("res/punk.png", &pb_style);
-    if (key_button_visible)
+    if (dialog_visible)
     {
-      punk_checkbox("Checkmate", &checked, NULL);
+      show_dialog();
     }
     else
     {
-      punk_checkbox("Checkmate1", &checked, NULL);
+      show_gui();
     }
-
-    if (punk_textbox(buf, NULL))
-    {
-      printf("Text changed: %s\n", buf);
-    }
-
-    punk_label("It's over", &lbl_style);
-    punk_label("9000!", &lbl_style);
-    if (punk_picture_button("res/punk.png", &pb_style)) printf("Punk!\n");
-
-    punk_end_layout(); // Vertical layout
-
-    if (punk_button("hello", NULL)) printf("Hello world!\n");
-
-    if (key_button_visible)
-    {
-      if (punk_button("maybe", NULL)) printf("Maybe!\n");
-    }
-    else
-    {
-      punk_skip_layout_widget();
-    }
-
-    if (checked)
-    {
-      punk_begin_vertical_layout("1:2:1", PUNK_FILL, PUNK_FILL);
-
-      if (punk_button("V1", &btn_style) == PUNK_CLICK_RIGHT) printf("V1!\n");
-      if (punk_button("V2", &btn_style)) printf("V2!\n");
-      if (punk_button("V3", &btn_style)) printf("Punk!\n");
-
-      punk_end_layout(); // Vertical layout
-    }
-
-    punk_end_layout(); // Horizontal layout
 
     punk_end();
 
